dd432e797a7f0a1cda5d8dcced42de0739a6b769
Tsuwagyu <runmikeyrun2@gmail.com>
2026-07-08 16:06:10 -0400

    add newNode reference and conditionals for linked list head

--- linkedList.js
+++ linkedList.js
@@ -1,6 +1,30 @@
-import Node from "../linked-lists/node"
+import Node from "./node"
 
 
-class linkedList() {
+export default class LinkedList {
+
+    constructor() {
+        this.head = null;
+    }
+
+    append(value) {
+        const newNode = Node(value);
+        if (this.head === null) {
+            return (this.head = newNode);
+        }
+
+        let pointer = this.head;
+        if (pointer !== null) {
+             // pointer should point to the nextNode// 
+             }
+    }
+
+
+    prepend(value) {
+        if (this.head !== null) {
+
+        }
+    }
 
 }
+
