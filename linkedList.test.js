1f2a565a725e061be29bb4f2b73f494bc499a913
Tsuwagyu <runmikeyrun2@gmail.com>
2026-07-09 14:49:41 -0400

    add happy path test for linked list

---
+++ linkedList.test.js
@@ -0,0 +1,30 @@
+import LinkedList from "./linkedList";
+
+// describe because these are all tests for the linked list class
+
+describe('LinkedList', () => {
+    let linkedList;
+
+    beforeEach(() => {
+        linkedList = new LinkedList();
+    });
+
+
+    // check head and new list must be empty, append create node at front, prepend insert new node in front of old nod
+    test('should create an empty linked list', () => {
+        expect(linkedList.head).toBeNull();
+    });
+
+    test('append adds a value when the list is empty', () => {
+        linkedList.append(10);
+        expect(linkedList.head.value).toBe(10);
+        expect(linkedList.head.nextNode).toBeNull();
+    });
+
+    test('prepend adds a value to the beginning of the list', () => {
+        linkedList.append(20);
+        linkedList.prepend(10);
+        expect(linkedList.head.value).toBe(10);
+        expect(linkedList.head.nextNode.value).toBe(20);
+    });
+});     
